26e0d5a2b1fd501339f6874d2bc8d42029753ac3
raresica1234 <colorlord98@gmail.com>
2018-07-17 15:35:55 +0300

    Created Logger

---
+++ app/src/main/java/fuck/it/attack/core/Logger.java
@@ -0,0 +1,33 @@
+package fuck.it.attack.core;
+
+import android.util.Log;
+
+/**
+ * Created by Rares on 17-Jul-18.
+ */
+
+public class Logger {
+
+	public static final String TAG = "Attack";
+
+	public static void error(String message) {
+		Log.e(TAG, message);
+	}
+
+	public static void info(String message) {
+		Log.i(TAG, message);
+	}
+
+	public static void warn(String message) {
+		Log.w(TAG, message);
+	}
+
+	public static void msg(String message) {
+		Log.v(TAG, message);
+	}
+
+	public static void debug(String message) {
+		Log.d(TAG, message);
+	}
+
+}
