4a487937c0205d99813494fb4043956ae68c9cd4
itay2805 <itay2805@protonmail.com>
2018-07-17 21:52:48 +0300

    now you can do shit

--- app/src/main/java/fuck/it/attack/MainActivity.java
+++ app/src/main/java/fuck/it/attack/MainActivity.java
@@ -45,67 +45,67 @@ public class MainActivity extends Activity {
 
 		context = this;
 
-		final GoogleSignInClient signInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+		context.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		context.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		FileUtils.assetManager = getAssets();
+
+		ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+		ConfigurationInfo  info = am.getDeviceConfigurationInfo();
+		boolean supportES3 = (info.reqGlEsVersion >= 0x30000);
+		if(supportES3) {
+			mainRenderer = new MainRenderer();
+			MainSurfaceView mainSurfaceView = new MainSurfaceView(context);
+			mainSurfaceView.setEGLContextClientVersion(3);
+			mainSurfaceView.setRenderer(mainRenderer);
+
+			leftJoystick = new JoystickView(context);
+			rightJoystick = new JoystickView(context);
+
+			DisplayMetrics display = new DisplayMetrics();
+			((Activity) MainActivity.getContext()).getWindowManager()
+					.getDefaultDisplay()
+					.getMetrics(display);
+
+			int size = display.widthPixels / 5;
+
+			FrameLayout.LayoutParams leftLayoutParams=new FrameLayout.LayoutParams(size, size);
+			leftLayoutParams.setMargins(20, display.heightPixels - size - 20, 0, 0);
+			leftJoystick.setLayoutParams(leftLayoutParams);
+
+			FrameLayout.LayoutParams rightLayoutParams=new FrameLayout.LayoutParams(size, size);
+			rightLayoutParams.setMargins(display.widthPixels - size - 20, display.heightPixels - size - 20, 0, 0);
+			rightJoystick.setLayoutParams(rightLayoutParams);
+
+			FrameLayout layout = new FrameLayout(context);
+			layout.addView(mainSurfaceView);
+			layout.addView(leftJoystick);
+			layout.addView(rightJoystick);
+
+			context.setContentView(layout);
+		}else {
+			new AlertDialog.Builder(context).setMessage("Your device doesn't support ES3. (\" + info.reqGlEsVersion + \")").setNeutralButton("Exit", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					System.exit(0);
+				}
+			}).show();
+		}
+
+		/*final GoogleSignInClient signInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
 		signInClient.silentSignIn().addOnCompleteListener(this, new OnCompleteListener<GoogleSignInAccount>() {
 			@Override
 			public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
 				if (task.isSuccessful()) {
-					Logger.info("Logged in " + task.getResult().getDisplayName());
-
 					account = task.getResult();
 
-					context.requestWindowFeature(Window.FEATURE_NO_TITLE);
-					context.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-					FileUtils.assetManager = getAssets();
-
-					ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-					ConfigurationInfo  info = am.getDeviceConfigurationInfo();
-					boolean supportES3 = (info.reqGlEsVersion >= 0x30000);
-					if(supportES3) {
-						mainRenderer = new MainRenderer();
-						MainSurfaceView mainSurfaceView = new MainSurfaceView(context);
-						mainSurfaceView.setEGLContextClientVersion(3);
-						mainSurfaceView.setRenderer(mainRenderer);
-
-						leftJoystick = new JoystickView(context);
-						rightJoystick = new JoystickView(context);
-
-						DisplayMetrics display = new DisplayMetrics();
-						((Activity) MainActivity.getContext()).getWindowManager()
-								.getDefaultDisplay()
-								.getMetrics(display);
-
-						int size = display.widthPixels / 5;
-
-						FrameLayout.LayoutParams leftLayoutParams=new FrameLayout.LayoutParams(size, size);
-						leftLayoutParams.setMargins(20, display.heightPixels - size - 20, 0, 0);
-						leftJoystick.setLayoutParams(leftLayoutParams);
-
-						FrameLayout.LayoutParams rightLayoutParams=new FrameLayout.LayoutParams(size, size);
-						rightLayoutParams.setMargins(display.widthPixels - size - 20, display.heightPixels - size - 20, 0, 0);
-						rightJoystick.setLayoutParams(rightLayoutParams);
-
-						FrameLayout layout = new FrameLayout(context);
-						layout.addView(mainSurfaceView);
-						layout.addView(leftJoystick);
-						layout.addView(rightJoystick);
-
-						context.setContentView(layout);
-					}else {
-						new AlertDialog.Builder(context).setMessage("Your device doesn't support ES3. (\" + info.reqGlEsVersion + \")").setNeutralButton("Exit", new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								System.exit(0);
-							}
-						}).show();
-					}
+					Logger.info("Logged in " + account.getDisplayName());
 				} else {
 					Intent intent = signInClient.getSignInIntent();
 					startActivityForResult(intent, SIGN_IN_RESULT);
 				}
 			}
-		});
+		});*/
 	}
 
 	public static JoystickView getLeftJoystick() {
@@ -126,8 +126,7 @@ public class MainActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 
-
-		final GoogleSignInClient signInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+		/*final GoogleSignInClient signInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
 		signInClient.silentSignIn().addOnCompleteListener(this, new OnCompleteListener<GoogleSignInAccount>() {
 			@Override
 			public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
@@ -135,11 +134,14 @@ public class MainActivity extends Activity {
 					account = task.getResult();
 					Logger.info("Logged in " + account.getDisplayName());
 				} else {
-					Intent intent = signInClient.getSignInIntent();
-					startActivityForResult(intent, SIGN_IN_RESULT);
+					if(!context.isFinishing())
+					{
+						Intent intent = signInClient.getSignInIntent();
+						startActivityForResult(intent, SIGN_IN_RESULT);
+					}
 				}
 			}
-		});
+		});*/
 	}
 
 	@Override
@@ -153,12 +155,16 @@ public class MainActivity extends Activity {
 				Logger.info("Logged in " + account.getDisplayName());
 			}else {
 				String message = result.getStatus().getStatusMessage();
-				new AlertDialog.Builder(this).setMessage(message != null ? message : "You must sign-in to in order to play the game").setNeutralButton("Exit", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						System.exit(0);
-					}
-				}).show();
+
+				if(!context.isFinishing())
+				{
+					new AlertDialog.Builder(this).setMessage(message != null ? message : "You must sign-in to in order to play the game").setNeutralButton("Exit", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							System.exit(0);
+						}
+					}).show();
+				}
 			}
 		}
 	}
