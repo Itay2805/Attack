860c4da0fb785cc93fa64ecda8a812663ef34c3c
raresica1234 <colorlord98@gmail.com>
2018-07-20 14:11:10 +0300

    Chaining constructors and added uvs for spritesheet usage

--- app/src/main/java/fuck/it/attack/graphics/Sprite.java
+++ app/src/main/java/fuck/it/attack/graphics/Sprite.java
@@ -10,29 +10,34 @@ public class Sprite {
 	public Texture texture;
 	public boolean hasTexture = false;
 
+	private Vector2f uv1;
+	private Vector2f uv2;
 
 	public Sprite(float x, float y, float width, float height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		uv1 = new Vector2f(0, 0);
+		uv2 = new Vector2f(1, 1);
 	}
 
 	public Sprite(float x, float y, float width, float height, Color color) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+		this(x, y, width, height);
 		this.color = color;
 		hasTexture = false;
 	}
 
 	public Sprite(float x, float y, float width, float height, Texture texture) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+		this(x, y, width, height);
 		this.texture = texture;
+		hasTexture = true;
+	}
+
+	public Sprite(float x, float y, float width, float height, SpriteSheet sheet, int spritePosX, int spritePosY) {
+		this(x, y, width, height, sheet.getTexture());
+		uv1 = sheet.getUv1(spritePosX, spritePosY);
+		uv2 = sheet.getUv2(spritePosX, spritePosY);
 		hasTexture = true;
 	}
 
@@ -47,15 +52,15 @@ public class Sprite {
 	}
 
 	public Vector2f getUv1() {
-		return new Vector2f(0, 0);
+		return uv1;
 	}
 
 	public Vector2f getUv2() {
-		return new Vector2f(1, 1);
+		return uv2;
 	}
 
 	public float getColorFloat() {
-		if(color == null) {
+		if (color == null) {
 			return 0.0f;
 		} else {
 			return color.toFloat();
