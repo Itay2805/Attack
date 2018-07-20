f3beb35bea9616c13d989425e184e7e9514ad066
raresica1234 <colorlord98@gmail.com>
2018-07-20 12:16:29 +0300

    Added texture to sprite

--- app/src/main/java/fuck/it/attack/graphics/Sprite.java
+++ app/src/main/java/fuck/it/attack/graphics/Sprite.java
@@ -5,7 +5,9 @@ public class Sprite {
 	public float x, y;
 	public float width, height;
 	public Color color;
-	//public Texture texture;
+	public Texture texture;
+	public boolean hasTexture = false;
+
 
 	public Sprite(float x, float y, float width, float height) {
 		this.x = x;
@@ -20,5 +22,25 @@ public class Sprite {
 		this.width = width;
 		this.height = height;
 		this.color = color;
+		hasTexture = false;
+	}
+
+	public Sprite(float x, float y, float width, float height, Texture texture) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.texture = texture;
+		hasTexture = true;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+		hasTexture = false;
+	}
+
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+		hasTexture = true;
 	}
 }
