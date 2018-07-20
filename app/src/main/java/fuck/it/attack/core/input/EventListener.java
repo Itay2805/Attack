e7f4f3c76fc2b981bd73ccf86486e5ec5842a895
raresica1234 <colorlord98@gmail.com>
2018-07-20 17:06:48 +0300

    Added Event Listener

---
+++ app/src/main/java/fuck/it/attack/core/input/EventListener.java
@@ -0,0 +1,46 @@
+package fuck.it.attack.core.input;
+
+import android.view.MotionEvent;
+
+public class EventListener {
+
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onLongPress(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		return false;
+	}
+
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		return false;
+	}
+
+	public boolean onShowPress(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onDoubleTap(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onDoubleTapEvent(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+		return false;
+	}
+
+	public boolean onContextClick(MotionEvent e) {
+		return false;
+	}
+}
