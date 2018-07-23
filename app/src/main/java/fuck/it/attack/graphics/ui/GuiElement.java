b68381c6e2c9dea30d017bc918313cd05370e70a
raresica1234 <colorlord98@gmail.com>
2018-07-23 14:19:06 +0300

    GuiElement is not abstract and extends EventListener

--- app/src/main/java/fuck/it/attack/graphics/ui/GuiElement.java
+++ app/src/main/java/fuck/it/attack/graphics/ui/GuiElement.java
@@ -1,19 +1,28 @@
 package fuck.it.attack.graphics.ui;
 
+import org.joml.Vector2f;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import fuck.it.attack.core.input.Event;
+import fuck.it.attack.core.input.EventListener;
 import fuck.it.attack.graphics.Color;
 import fuck.it.attack.graphics.Renderer;
 import fuck.it.attack.graphics.Sprite;
 
-public abstract class GuiElement {
+public class GuiElement extends EventListener {
 
 	protected List<Sprite> spriteList = new ArrayList<>();
-
 	protected Color backgroundColor;
+	protected boolean visible = true;
+	protected Vector2f position;
+	protected Vector2f size;
 
-	protected GuiElement(float x, float y, float width, float height) {
+	protected GuiElement(String name, float x, float y, float width, float height) {
+		super(name);
+		position = new Vector2f(x, y);
+		size = new Vector2f(width, height);
 		spriteList.add(new Sprite(x, y, width, height));
 	}
 
@@ -24,5 +33,13 @@ public abstract class GuiElement {
 	public void setBackgroundColor(Color backgroundColor) {
 		spriteList.get(0).setColor(backgroundColor);
 		this.backgroundColor = backgroundColor;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public boolean contains(Event pos) {
+		return (pos.x >= position.x) && (pos.y >= position.y) && (pos.x <= size.x + position.x) && (pos.y <= size.y + position.y);
 	}
 }
