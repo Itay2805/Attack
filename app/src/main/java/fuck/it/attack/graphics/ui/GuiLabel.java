f3ee9c482ddde71d788bbe5c2e969cdbfece5fe7
raresica1234 <colorlord98@gmail.com>
2018-07-23 14:23:43 +0300

    Fixed GuiLabel and added onDown

--- app/src/main/java/fuck/it/attack/graphics/ui/GuiLabel.java
+++ app/src/main/java/fuck/it/attack/graphics/ui/GuiLabel.java
@@ -1,20 +1,31 @@
 package fuck.it.attack.graphics.ui;
 
+import fuck.it.attack.core.Logger;
+import fuck.it.attack.core.input.Event;
 import fuck.it.attack.graphics.Font;
 import fuck.it.attack.graphics.Sprite;
 
 public class GuiLabel extends GuiElement {
 
-    public GuiLabel(float x, float y, String text, Font font) {
-        super(x, y, font.getSize(), font.getSize() * text.length());
+	public GuiLabel(String name, float x, float y, String text, Font font) {
+		super(name, x, y, (font.getSize() + 3) * text.length(), font.getSize());
 
-        for(char c : text.toCharArray()) {
-            Sprite sprite = new Sprite(font.getCharSprite(c));
-            sprite.x = x;
-            sprite.y = y;
-            spriteList.add(sprite);
-            x += font.getSize() + 3;
-        }
-    }
+		float modX = x;
 
+		for (char c : text.toCharArray()) {
+			Sprite sprite = new Sprite(font.getCharSprite(c));
+			sprite.x = modX;
+			sprite.y = y;
+			spriteList.add(sprite);
+			modX += font.getSize() + 3;
+		}
+	}
+
+	@Override
+	public boolean onDown(Event e) {
+		// no need for checking if it's inside the bounds since if it's not in the bounds
+		// then it won't be called
+		Logger.error("Click! " + e.x + " " + e.y);
+		return true;
+	}
 }
