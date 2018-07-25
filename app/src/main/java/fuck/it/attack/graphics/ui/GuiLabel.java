2a4f752888148c0c944de2b9bf0010dcd768c7f0
itay2805 <itay2805@protonmail.com>
2018-07-25 22:14:05 +0300

    Made the text format in its own class

--- app/src/main/java/fuck/it/attack/graphics/ui/GuiLabel.java
+++ app/src/main/java/fuck/it/attack/graphics/ui/GuiLabel.java
@@ -2,6 +2,8 @@ package fuck.it.attack.graphics.ui;
 
 import fuck.it.attack.core.Logger;
 import fuck.it.attack.core.input.Event;
+import fuck.it.attack.core.text.TextFormat;
+import fuck.it.attack.core.text.Token;
 import fuck.it.attack.graphics.Color;
 import fuck.it.attack.graphics.Font;
 import fuck.it.attack.graphics.Sprite;
@@ -21,14 +23,37 @@ public class GuiLabel extends GuiElement {
 		float modX = x;
 		float modY = y;
 		Color original = color;
-		boolean skip = false;
 
-		for (int i = 0; i < text.length(); i++) {
-			char c = text.charAt(i);
-			switch(c) {
-				// special escape sequence
-				case '#': {
-					if(text.length() >= i || text.charAt(i + 1) != '[') {
+		for (Token token : TextFormat.parse(text)) {
+			Logger.info(token);
+			switch(token.getType()) {
+				case SPECIAL: {
+					switch(token.getToken()) {
+						case "reset": {
+							color = original;
+						} break;
+
+						default: {
+							color = Color.getColor(token.getToken());
+						} break;
+					}
+				} break;
+
+				case TAB: {
+					modX += size * 4 + 3;
+				} break;
+
+				case NEW_LINE: {
+					modY -= size + 3;
+					modX = x;
+				} break;
+
+				case SPACE: {
+					modX += size + 3;
+				} break;
+
+				case TEXT: {
+					for(char c : token.getToken().toCharArray()) {
 						Sprite sprite = new Sprite(font.getCharSprite(c));
 						sprite.x = modX;
 						sprite.y = modY;
@@ -37,53 +62,8 @@ public class GuiLabel extends GuiElement {
 						sprite.color = color;
 						spriteList.add(sprite);
 						modX += size + 3;
-						continue;
 					}
-					int end = text.indexOf(']', i);
-					if(end == -1) {
-						throw new IllegalStateException("Missing closing ] for special escape sequence in label. `" + text + "` at position " + i + "");
-					}
-					String colorName = text.substring(i + 2, end);
-					Logger.debug(colorName);
-					switch(colorName) {
-						case "reset": {
-							color = original;
-						} break;
-						default: {
-							color = Color.getColor(colorName);
-						} break;
-					}
-					i += colorName.length() + 2;
 				} break;
-
-				// tab
-				case '\t': {
-					modX += size * 4 + 3;
-				} break;
-
-				// newline
-				case '\n': {
-					modY += size + 3;
-					modX = x;
-				} break;
-
-				// space
-				case ' ': {
-					modX += size + 3;
-					// do nothing
-				} break;
-
-				// any other char
-				default: {
-					Sprite sprite = new Sprite(font.getCharSprite(c));
-					sprite.x = modX;
-					sprite.y = modY;
-					sprite.width = size;
-					sprite.height = size;
-					sprite.color = color;
-					spriteList.add(sprite);
-					modX += size + 3;
-				}
 			}
 		}
 	}
